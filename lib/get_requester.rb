7f7e938daa77ecdb16ffde33610c020f0f0d5538
Daniel Gould <dmgould310@gmail.com>
2020-08-14 12:51:08 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -19,22 +19,6 @@ class GetRequester
   
   def parse_json
     programs = JSON.parse(self.get_response_body)
-    
-    
   end 
-  
-  
-# def program_school
-# # we use the JSON library to parse the API response into nicely formatted JSON
-#   programs = JSON.parse(self.get_programs)
-#   programs.collect do |program|
-#     program["agency"]  
-#   end
-# end
-  
-  
-  
-  
-  
   
 end 
